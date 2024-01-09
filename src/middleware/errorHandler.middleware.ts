e867c32aef4d3837e8893977c7cb4ffe08fb8e68
Jeewanghimire31 <jeewanghimire31@gmail.com>
2024-01-09 11:27:36 +0545

    feat: errorHandler middleware

---
+++ src/middleware/errorHandler.middleware.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpException } from "../exceptions";
+
+/**
+ * Error handler middleware
+ *
+ * @param error
+ * @param request
+ * @param response
+ * @param next
+ */
+export async function errorHandlerMiddleware(
+  error: HttpException,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const { statusCode, message } = error;
+
+//   if (response.headersSent) {
+//     return next(error);
+//   }
+
+  return response.status(statusCode || 500).json({
+    error: message || "Something went wrong!",
+  });
+}
+
+export function notFoundHandlerMiddleware(_req: Request, res: Response) {
+  return res.status(404).json({
+    error: "Not found!",
+  });
+}
