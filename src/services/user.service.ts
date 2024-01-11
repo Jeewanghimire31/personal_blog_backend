ab31f93710517af7e478c7954ca6bd1ce857ba27
Jeewanghimire31 <jeewanghimire31@gmail.com>
2024-01-11 23:53:45 +0545

    feat: api routes for user(Get,GetById, Update, Delete) also exception throw if user not found

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from "bcrypt";
 import User, { UserRole } from "../entities/User.entity";
+import { NotFoundException } from "../exceptions";
 
 class UserService {
   async createUser(
@@ -24,6 +25,42 @@ class UserService {
 
   async comparePasswords(candidatePassword: string, hashedPassword: string) {
     return bcrypt.compare(candidatePassword, hashedPassword);
+  }
+
+  async getUsers() {
+    //* SELECT * FROM users Works like this
+    const users = await User.find();
+    return users;
+  }
+
+  async getUserById(UserId: number) {
+    const user = await User.findOne({
+      where: {
+        user_id: UserId,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException("User not Found");
+    }
+    return user;
+  }
+
+  async updateUser(id: number, body: User) {
+    const user = await this.getUserById(id);
+    if (!user) {
+      throw new NotFoundException("User not Found");
+    }
+    User.merge(user, body);
+    return user.save();
+  }
+
+  async deleteUser(id: number) {
+    const user = await this.getUserById(id);
+    if (!user) {
+      throw new NotFoundException("User not Found");
+    } else {
+      return await user.remove();
+    }
   }
 }
 
